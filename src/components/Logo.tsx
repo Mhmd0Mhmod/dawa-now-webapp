d575d8a7bf7cb8b850f844c594b02ab169801be2
Mhmd0Mhmod <iammhmdmhmod@gmail.com>
2025-02-09 16:30:05 +0200

    update logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,10 +2,11 @@ import LogoNoText from "/public/assets/logoNoText.svg?react";
 
 Logo.defaultProps = {
   center: false,
-}
+};
 
+// function Logo({ center }: { center?: boolean }) {
 
-function Logo({ center }: { center?: boolean }) {
+function Logo() {
   return (
     <div className="relative flex items-center">
       <h1 className={`font-k2d text-7xl font-extrabold text-white`}>DawaNow</h1>
